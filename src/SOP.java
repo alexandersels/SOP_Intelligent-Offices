4c6aca17cab44ddf6796f7095f05848b336cd650
Alexander Sels <alexandersels@hotmail.com>
2014-02-14 15:58:10 +0100

    Test first commit

---
+++ src/SOP.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Alexander on 14/02/14.
+ */
+public class SOP {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hallo SOP!");
+    }
+
+    public int getNumber()
+    {
+        return 20;
+    }
+}
